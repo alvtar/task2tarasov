1462b844e36bf7e157382b842fdd4fc2f2485fa1
Администратор <@comp>
2016-11-14 23:16:51 +0300

    Word realized

--- src/reader/TextReader.java
+++ src/reader/TextReader.java
@@ -1,7 +1,9 @@
 package reader;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import domain.*;
 
 
 public class TextReader {
@@ -16,22 +18,38 @@ public class TextReader {
                     rlst = new ArrayList<String>(Arrays.asList(arr));
                     list.addLast(rlst);
               */  
-    try(FileReader reader = new FileReader("D:\\workspace\\task2tarasov\\bin\\reader\\book.txt"))
+    File f=new File("D:\\workspace\\task2tarasov\\bin\\reader\\book.txt");
+    
+    try(FileReader reader = new FileReader(f))
     {
        // читаем посимвольно
-        int c;
-        while((c=reader.read())!=-1){
+        //int c;
+        //while((c=reader.read())!=-1){
+        char[] buffer=new char[(int) f.length()];
+        
+        reader.read(buffer);
              
-            System.out.print((char)c);
+            String sss=new String(buffer);
+            String[] words=sss.split("\\s*(\\s|,|!|\\.)\\s*");
+        
+            //System.out.print(sss);
             
+            for (String word:words){
+                Word w=new Word(word);
+                for (Symbol www:w.symbols){
+                    System.out.print(www.letter);
+                }
+                System.out.println();
+            }
+
             
         } 
-    }
+    
     catch(IOException ex){
          
         System.out.println(ex.getMessage());
     }   
-} 
-
+ 
     }
+}
 

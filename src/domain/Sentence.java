45b71e4c7a972547e8c69fd4e048af1aa8d377ce
Администратор <@comp>
2016-11-20 16:15:12 +0300

    +++

--- src/domain/Sentence.java
+++ src/domain/Sentence.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class Sentence extends SymbolParser {
     public LinkedList<SymbolParser> elements;
-    protected static String delimPunctuation="[0-9a-zA-Zà-ÿÀ-ß]+";//"\\b\\W+\\b/g";//"[. ,?!:;'-=+]";//"\\b\\W+\\b/g";
+    protected static String delimPunctuation="[^. ,?!:;'-=+]";//"[0-9a-zA-Zà-ÿÀ-ß]";//|\\r+|\\n+";//\\f\\n\\r\\t\\v]+";//"\\b\\W+\\b/g";//"[. ,?!:;'-=+]";//"\\b\\W+\\b/g";
     //protected static String delimWords="[^a-zA-Zà-ÿÀ-ß0-9]";//;"[. ,?!:;'-=+]";//"\\b\\W+\\b/g";
     
     public Sentence(String inStr) {
@@ -16,7 +16,8 @@ public class Sentence extends SymbolParser {
     protected void parse() {
         elements=new LinkedList<>();
         
-        String[] punkt=inStr.toString().split(delimPunctuation);
+        String[] punkt=inStr.replaceAll(delimPunctuation,"").split(delimPunctuation);
+        //String[] punkt2=punkt.toString().split("\r|\n");
         
         for (String p:punkt){
             elements.add(new Punctuation(p));
@@ -24,13 +25,13 @@ public class Sentence extends SymbolParser {
             //s.
         }
         
-        StringBuilder outStr=new StringBuilder(inStr);//.replaceAll("[^a-zA-Zà-ÿÀ-ß0-9]", " "));//(matcher.replaceAll(""))
+        StringBuilder outStr=new StringBuilder(inStr);//.replaceAll("[^a-zA-Zà-ÿÀ-ß0-9]", " ");//(matcher.replaceAll(""))
         
-        String[] sents=outStr.toString().split("[^a-zA-Zà-ÿÀ-ß0-9]");
+        String[] sents=outStr.toString().split("[^a-zA-Zà-ÿÀ-ß0-9]");//|\\r|\\n+");
         
         for (String s:sents){
             elements.add(new Word(s));
-            //System.out.println(s);
+            System.out.println(s);
             //s.
         }
         
